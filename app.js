dabdf9d5b0a90a6ebcf765137a4dbcbd89313d27
kanwar ammar <kanwarammar95@gmail.com>
2022-06-29 23:48:01 +0500

    follow configuration

--- app.js
+++ app.js
@@ -9,13 +9,10 @@ var cookieParser = require("cookie-parser");
 
 const { SpotifyApp } = require("./spotify");
 
-const connect = mongoose.connect(
-  process.env.DB_CONNECTION_STRING,
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
+const connect = mongoose.connect(process.env.DB_CONNECTION_STRING, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 connect.then(
   (db) => {
     console.log("Connected correctly to server");
@@ -60,7 +57,113 @@ app
   .use(cors())
   .use(cookieParser());
 
+// app.get("/login", function (req, res) {
+//   var state = generateRandomString(16);
+//   res.cookie(stateKey, state);
 
+//   // your application requests authorization
+//   var scope = "user-read-private user-read-email";
+//   res.redirect(
+//     "https://accounts.spotify.com/authorize?" +
+//       querystring.stringify({
+//         response_type: "code",
+//         client_id: client_id,
+//         scope: scope,
+//         redirect_uri: redirect_uri,
+//         state: state,
+//       })
+//   );
+// });
+// app.get("/callback", function (req, res) {
+//   // your application requests refresh and access tokens
+//   // after checking the state parameter
+
+//   var code = req.query.code || null;
+//   var state = req.query.state || null;
+//   var storedState = req.cookies ? req.cookies[stateKey] : null;
+
+//   if (state === null || state !== storedState) {
+//     res.redirect(
+//       "/#" +
+//         querystring.stringify({
+//           error: "state_mismatch",
+//         })
+//     );
+//   } else {
+//     res.clearCookie(stateKey);
+//     var authOptions = {
+//       url: "https://accounts.spotify.com/api/token",
+//       form: {
+//         code: code,
+//         redirect_uri: redirect_uri,
+//         grant_type: "authorization_code",
+//       },
+//       headers: {
+//         Authorization:
+//           "Basic " +
+//           new Buffer(client_id + ":" + client_secret).toString("base64"),
+//       },
+//       json: true,
+//     };
+
+//     request.post(authOptions, function (error, response, body) {
+//       console.log(error);
+//       if (!error && response.statusCode === 200) {
+//         var access_token = body.access_token,
+//           refresh_token = body.refresh_token;
+
+//         var options = {
+//           url: "https://api.spotify.com/v1/me",
+//           headers: { Authorization: "Bearer " + access_token },
+//           json: true,
+//         };
+
+//         // use the access token to access the Spotify Web API
+//         request.get(options, function (error, response, body) {
+//           console.log(body);
+//         });
+
+//         // we can also pass the token to the browser to make requests from there
+//         res.redirect(
+//           "/#" +
+//             querystring.stringify({
+//               access_token: access_token,
+//               refresh_token: refresh_token,
+//             })
+//         );
+//       } else {
+//         res.redirect(
+//           "/#" +
+//             querystring.stringify({
+//               error: "invalid_token",
+//             })
+//         );
+//       }
+//     });
+//   }
+// });
+
+// const scopes = [
+//   "ugc-image-upload",
+//   "user-read-playback-state",
+//   "user-modify-playback-state",
+//   "user-read-currently-playing",
+//   "streaming",
+//   "app-remote-control",
+//   "user-read-email",
+//   "user-read-private",
+//   "playlist-read-collaborative",
+//   "playlist-modify-public",
+//   "playlist-read-private",
+//   "playlist-modify-private",
+//   "user-library-modify",
+//   "user-library-read",
+//   "user-top-read",
+//   "user-read-playback-position",
+//   "user-read-recently-played",
+//   "user-follow-read",
+//   "user-follow-modify",
+// ];
 
 app.use("/api/user", userRoutes);
 
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -9,15 +9,40 @@ router.get("/allusers", async function (req, res) {
   });
 });
 
-router.post("/follow", async function (req, res) {
-  console.log("follow");
-  const { userId,userInfo } = req.body;
-  const user = await User.findOne({ userId });
-  user.followers.push(userInfo);
-  user.save();
-  res.status(200).json({
-    user,
-  });
+router.post("/follow/:userSpotifyId", async function (req, res) {
+  try {
+    const { userSpotifyId } = req.params;
+    const { followUser } = req.body;
+    const user = await User.findOne({ userSpotifyId });
+    const userFollowed = await User.findOne({
+      userSpotifyId: followUser.userSpotifyId,
+    });
+    console.log(userSpotifyId, followUser.userSpotifyId);
+    if (userSpotifyId === followUser.userSpotifyId) {
+      return res.status(400).json({
+        message: "a user cannot follow himself",
+      });
+    }
+    for (var followers of user.following) {
+      if (followers.userSpotifyId === followUser.userSpotifyId) {
+        return res.status(400).json({
+          message: "user is already followed",
+        });
+      }
+    }
+    user.following.push(followUser);
+    userFollowed.followers.push(user);
+    user.save();
+    userFollowed.save();
+    res.status(200).json({
+      message: "user followed successfully",
+      data: user,
+    });
+  } catch (err) {
+    res.status(400).json({
+      message: err.message,
+    });
+  }
 });
 
 module.exports = router;
